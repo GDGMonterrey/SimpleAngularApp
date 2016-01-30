90dcec160e2a4dbb6e7873666cbbaf04cff10214
Joel Humberto Gómez Paredes <elmaildeldezkareid@gmail.com>
2016-01-30 16:55:21 -0600

    Esqueleto, simple app en angular

---
+++ public/js/app.routes.js
@@ -0,0 +1,23 @@
+angular.module('routerRoutes', ['ngRoute'])
+// configure our routes
+.config(function($routeProvider, $locationProvider) { 
+	$routeProvider
+		.when('/', {
+  		templateUrl : 'views/pages/home.html',
+  		controller  : 'homeController',
+  		controllerAs: 'home'
+  	})
+    .when('/about', {
+	    templateUrl : 'views/pages/about.html',
+      controller  : 'aboutController',
+      controllerAs: 'about'
+    })
+    // route for the contact page
+    .when('/contact', {
+      templateUrl : 'views/pages/contact.html',
+      controller  : 'contactController',
+      controllerAs: 'contact'
+    });
+    // set our app up to have pretty URLS
+	$locationProvider.html5Mode(true);
+});
